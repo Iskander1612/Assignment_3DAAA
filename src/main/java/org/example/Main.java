92e97b73eb2fe3a780f882a385d94a5d82d9b58f
Iskander <Assa>
2025-10-25 19:03:35 +0500

    Algorithms

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.algorithms.KruskalAlgorithm;
+import org.example.algorithms.PrimAlgorithm;
+import org.example.graph.Graph;
+import org.example.model.MSTResult;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Graph g = new Graph();
+        g.addEdge("A","B",4);
+        g.addEdge("A","C",1);
+        g.addEdge("B","C",2);
+        g.addEdge("B","D",5);
+        g.addEdge("C","D",3);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        PrimAlgorithm prim = new PrimAlgorithm();
+        KruskalAlgorithm kruskal = new KruskalAlgorithm();
+
+        MSTResult r1 = prim.run(g);
+        MSTResult r2 = kruskal.run(g);
+
+        System.out.println("Prim   cost=" + r1.totalCost + " ops=" + r1.operations + " ms=" + r1.timeMs);
+        System.out.println("Kruskal cost=" + r2.totalCost + " ops=" + r2.operations + " ms=" + r2.timeMs);
+        System.out.println("Prim MST: " + r1.mstEdges);
+        System.out.println("Kruskal MST: " + r2.mstEdges);
     }
 }
